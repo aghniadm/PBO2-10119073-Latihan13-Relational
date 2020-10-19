c967ac744ff3ac611424b1904b08ad90ab19eb40
aghniadm <aghniadewii@gmail.com>
2020-10-19 09:01:28 +0700

    PBO2-10119073-Latihan13-Relational

---
+++ src/pbo2/pkg10119073/latihan13/relational/PBO210119073Latihan13Relational.java
@@ -0,0 +1,26 @@
+package pbo2.pkg10119073.latihan13.relational;
+
+/**
+ *
+ * @author 
+ * NAMA     : Aghnia Dewi Mahiranie
+ * KELAS    : PBO2
+ * NIM      : 10119073
+ * Deskripsi Program : Program ini berisi program untuk mengimplementasikan
+ * relational
+ *
+ */
+public class PBO210119073Latihan13Relational {
+
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b  = " + (bil1 > bil2));
+        System.out.println("a < b  = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
